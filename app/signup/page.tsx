22c14dc2599f8f4742945f5982e7e290c4b5b123
Abrham <abrsh6266@gmail.com>
2024-03-19 13:33:44 -0800

    updated sign up page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -4,59 +4,63 @@ import logo from "/public/images/logo.png";
 
 const SignUp = () => {
   return (
-    <div className="h-screen flex justify-center items-center bg-gray-100">
-      <div className="mx-auto p-8 bg-white border-4 border-gray-600 rounded-lg flex">
-        <div className="text-center px-8 py-12">
-          <a className="mb-6" href="#">
-            <Image
-              style={{
-                maxWidth: "100%",
-                height: "100%",
-              }}
-              src={logo}
-              alt="logo"
-            />
-          </a>
-        </div>
-        <form action="" className="flex flex-col justify-center px-8 py-6">
-          <div className="mb-6">
-            <input
-              className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-              type="text"
-              placeholder="Full Name"
-            />
-          </div>
-          <div className="mb-6">
-            <input
-              className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-              type="email"
-              placeholder="Email"
-            />
-          </div>
-          <div className="mb-6">
-            <input
-              className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-              type="password"
-              placeholder="password"
-            />
-          </div>
-          <div className="mb-6">
-            <input
-              className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-              type="password"
-              placeholder="confirm Password"
-            />
-          </div>
-          <button className="py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-black hover:bg-gray-600 border-3 border-indigo-900 shadow rounded transition duration-200">
-            Sign Up
-          </button>
-          <p className="text-center font-extrabold">
-            Already have an account?{" "}
-            <a className="text-blue-500 hover:underline" href="/login">
-              Login here
+    <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
+      <div className="max-w-screen-xl m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1 flex-col sm:flex-row">
+        <div className="sm:w-1/2 p-6 sm:p-12 flex justify-center">
+          <div className="text-center px-8 py-12">
+            <a className="mb-6" href="#">
+              <Image
+                style={{
+                  width: "auto",
+                  height: "100%",
+                }}
+                src={logo}
+                alt="logo"
+              />
             </a>
-          </p>
-        </form>
+          </div>
+        </div>
+        <div className="sm:w-1/2 p-6 sm:p-12">
+          <form action="" className="flex flex-col justify-center px-8 py-6">
+            <div className="mb-6">
+              <input
+                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
+                type="text"
+                placeholder="Full Name"
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
+                type="email"
+                placeholder="Email"
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
+                type="password"
+                placeholder="Password"
+              />
+            </div>
+            <div className="mb-6">
+              <input
+                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
+                type="password"
+                placeholder="Confirm Password"
+              />
+            </div>
+            <button className="py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-black hover:bg-gray-600 border-3 border-indigo-900 shadow rounded transition duration-200">
+              Sign Up
+            </button>
+            <p className="text-center font-extrabold">
+              Already have an account?{" "}
+              <a className="text-blue-500 hover:underline" href="/login">
+                Login here
+              </a>
+            </p>
+          </form>
+        </div>
       </div>
     </div>
   );
