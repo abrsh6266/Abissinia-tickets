3fbb254ea60276133da7d1cb7882a2097c401eac
Abrham <abrsh6266@gmail.com>
2024-03-23 13:21:46 -0800

    some refactors

--- app/components/MovieDetail.tsx
+++ app/components/MovieDetail.tsx
@@ -32,13 +32,12 @@ const MovieDetail = ({
   return (
     <>
       <div className="container m-auto md:grid lg:grid md:pl-4 md:grid-cols-5 lg:grid-cols-8 gap-4">
-        <div className="col-span-full my-2">
-          <a
+        <div className="ml-2 col-span-full my-2">
+          <h1
             className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
-            href="#"
           >
             Movie Detail
-          </a>
+          </h1>
         </div>
         <div className="relative tile row-start-2 row-end-5 col-span-1 md:col-span-2 lg:col-span-3 max-h-96 h-96 mt-8">
           <Image
@@ -50,13 +49,12 @@ const MovieDetail = ({
         <div className="row-start-4 row-end-5 md:row-start-2 md:row-end-3 col-span-2 md:col-span-3 lg:col-span-5">
           <MovieInfoCard {...movie} />
         </div>
-        <div className="col-span-full my-2">
-          <a
+        <div className="ml-2 col-span-full my-2">
+          <h1
             className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
-            href="#"
           >
             Movie Schedule
-          </a>
+          </h1>
         </div>
       </div>
       <div className="m-auto lg:flex sm: gap-4">
