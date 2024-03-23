895314251eb744d39c144f414a1052ee37eaea0b
Abrham <abrsh6266@gmail.com>
2024-03-22 23:42:02 -0800

    updated navbar

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -60,9 +60,9 @@ const Navbar = () => {
                 <Link
                   className={`${
                     link.href === currentPath
-                      ? "text-blue-500 text-2xl"
-                      : "text-gray-400"
-                  } text-lg hover:text-gray-500 transition duration-300 ease-in-out`}
+                      ? "text-green-500 text-2xl"
+                      : ""
+                  } p-4 border-b-2 border-green-500 border-opacity-0 hover:border-opacity-100 hover:text-green-500 duration-200 cursor-pointer`}
                   href={link.href}
                 >
                   {link.label}
