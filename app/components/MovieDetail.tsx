fcb9172141f3fe0fed1a90bfba18b4063b7a6ce6
Abrham <abrsh6266@gmail.com>
2024-03-23 13:18:01 -0800

    adding movie schedule component

--- app/components/MovieDetail.tsx
+++ app/components/MovieDetail.tsx
@@ -3,6 +3,7 @@
 import Image from "next/image";
 import { Movie } from "../data";
 import MovieInfoCard from "./MovieInfoCard";
+import MovieSchedule from "./MovieSchedule";
 
 const MovieDetail = ({
   title,
@@ -29,44 +30,50 @@ const MovieDetail = ({
     showTime,
   };
   return (
-    <div className="container m-auto md:grid lg:grid md:pl-4 md:grid-cols-5 lg:grid-cols-8 gap-4">
-      <div className="col-span-full my-2">
-      <a
-              className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
-              href="#"
-            >
-              Movie Detail
-            </a>
+    <>
+      <div className="container m-auto md:grid lg:grid md:pl-4 md:grid-cols-5 lg:grid-cols-8 gap-4">
+        <div className="col-span-full my-2">
+          <a
+            className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
+            href="#"
+          >
+            Movie Detail
+          </a>
+        </div>
+        <div className="relative tile row-start-2 row-end-5 col-span-1 md:col-span-2 lg:col-span-3 max-h-96 h-96 mt-8">
+          <Image
+            className="absolute top-0 left-0 w-full h-full object-cover"
+            src={poster}
+            alt={title}
+          />
+        </div>
+        <div className="row-start-4 row-end-5 md:row-start-2 md:row-end-3 col-span-2 md:col-span-3 lg:col-span-5">
+          <MovieInfoCard {...movie} />
+        </div>
+        <div className="col-span-full my-2">
+          <a
+            className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl "
+            href="#"
+          >
+            Movie Schedule
+          </a>
+        </div>
       </div>
-      <div className="relative tile row-start-2 row-end-5 col-span-1 md:col-span-2 lg:col-span-3 max-h-96 h-96 mt-8">
-        <Image
-          className="absolute top-0 left-0 w-full h-full object-cover"
-          src={poster}
-          alt={title}
-        />
+      <div className="m-auto lg:flex sm: gap-4">
+      <div className="lg:col-start-4 lg:col-span-2 my-4 mx-4">
+        <h1 className="tile-marker">Monday</h1>
+        <MovieSchedule showTime={showTime}/>
       </div>
-      <div className="row-start-4 row-end-5 md:row-start-2 md:row-end-3 col-span-2 md:col-span-3 lg:col-span-5">
-        <MovieInfoCard {...movie} />
+      <div className="lg:col-start-4 lg:col-span-2 my-4 mx-4">
+        <h1 className="tile-marker">Tuesday</h1>
+        <MovieSchedule showTime={showTime}/>
       </div>
-      <div className="tile bg-lime-600 lg:col-start-4 lg:col-span-2">
-        <h1 className="tile-marker">FOUR</h1>
+      <div className="lg:col-start-4 lg:col-span-2 my-4 mx-4">
+        <h1 className="tile-marker">Wednesday</h1>
+        <MovieSchedule showTime={showTime}/>
       </div>
-      <div className="tile bg-green-600">
-        <h1 className="tile-marker">FIVE</h1>
       </div>
-      <div className="tile bg-emerald-500">
-        <h1 className="tile-marker">SIX</h1>
-      </div>
-      <div className="tile bg-teal-500">
-        <h1 className="tile-marker">SEVEN</h1>
-      </div>
-      <div className="tile bg-purple-500">
-        <h1 className="tile-marker">EIGHT</h1>
-      </div>
-      <div className="tile bg-pink-500 row-start-5 md:col-span-full">
-        <h1 className="tile-marker">NINE</h1>
-      </div>
-    </div>
+    </>
   );
 };
 
---
+++ app/components/MovieSchedule.tsx
@@ -0,0 +1,38 @@
+interface Props {
+  showTime: string[];
+}
+const MovieSchedule = ({ showTime }: Props) => {
+  return (
+    <div className="mt-10 border-2 rounded p-2">
+      <div className="flex items-center justify-between">
+        <h3 className="text-sm font-medium text-gray-900">Time</h3>
+        <p
+          className="text-sm font-medium text-indigo-600 hover:text-indigo-500"
+        >
+          3/28/2024
+        </p>
+      </div>
+
+      <fieldset className="mt-4">
+        <legend className="sr-only">Choose the time</legend>
+        <div className="flex gap-4 lg:grid-cols-4">
+            {
+                showTime.map(time=>{
+                    return <button key={time} className="overflow-hidden p-2 h-auto bg-black text-white border-none rounded-md text-xl font-bold cursor-pointer relative z-10 group">
+                    {time}
+                    <span className="absolute w-36 h-32 -top-8 -left-2 bg-white rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-500 duration-1000 origin-left"></span>
+                    <span className="absolute w-36 h-32 -top-8 -left-2 bg-purple-400 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-700 duration-700 origin-left"></span>
+                    <span className="absolute w-36 h-32 -top-8 -left-2 bg-purple-600 rotate-12 transform scale-x-0 group-hover:scale-x-100 transition-transform group-hover:duration-1000 duration-500 origin-left"></span>
+                    <span className="group-hover:opacity-100 group-hover:duration-1000 duration-100 opacity-0 absolute top-2 left-3 z-10">
+                      Choose
+                    </span>
+                  </button>
+                })
+            }
+        </div>
+      </fieldset>
+    </div>
+  );
+};
+
+export default MovieSchedule;
