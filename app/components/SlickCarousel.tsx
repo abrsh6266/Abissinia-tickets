857e8d67952bc56d739ad517235f9c4d2ecd19ec
Abrham <abrsh6266@gmail.com>
2024-03-28 13:25:05 -0800

    adding search component

--- app/components/SlickCarousel.tsx
+++ app/components/SlickCarousel.tsx
@@ -5,6 +5,7 @@ import "slick-carousel/slick/slick-theme.css";
 import { list } from "../data";
 import { useState } from "react";
 import Image from "next/image";
+import Search from "./Search";
 
 const SlickCarousel = () => {
   const [people, setPeople] = useState(list);
@@ -19,17 +20,18 @@ const SlickCarousel = () => {
     pauseOnHover: true,
   };
   return (
-    <section className=" overflow-hidden bg-gradient-to-r from-gray-500 via-gray-500 to-gray-500">
+    <section className=" overflow-hidden bg-gradient-to-b from-white via-gray-500 to-gray-500">
+      <Search />
       <Slider {...settings} >
         {people.map((p) => {
           const { name, image, id, quote, title } = p;
           return (
             <article key={id}>
-              <div className="relative w-full h-96 mb-4 border-4 border-primary-200 shadow-lg">
+              <div className="bg-gradient-to-b from-gray-400 via-gray-500 to-gray-500 relative w-full h-96 mb-4 mx-1 rounded-lg shadow-lg">
                 <Image
                   src={image}
                   alt={name}
-                  className="object-cover w-full h-full"
+                  className="bg-gray-500 object-cover w-full h-full"
                 />
                 <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center text-white bg-black bg-opacity-50">
                   <p className="text-xl">{quote}</p>
