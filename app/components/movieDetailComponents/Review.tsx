5771d2272994e1da2dfb5b9cebb2c6c5251d4fba
Abrham <abrsh6266@gmail.com>
2024-04-10 13:55:12 -0800

    some refactors with see more comments

--- app/components/movieDetailComponents/Review.tsx
+++ app/components/movieDetailComponents/Review.tsx
@@ -1,13 +1,34 @@
+"use client";
+import { useState } from "react";
 import Message from "./Message";
 
 const Review = () => {
+  const [showComment, setShowComment] = useState(false);
+  const [visibleMessages, setVisibleMessages] = useState(1);
+  const totalMessages = 10;
+  const messages = new Array(totalMessages).fill(<Message />);
+
+  const handleSeeMore = () => {
+    setVisibleMessages(Math.min(visibleMessages + 3, totalMessages));
+  };
+
+  const handleShowLess = () => {
+    setVisibleMessages(1);
+  };
+
   return (
-    <div>
+    <div className=" overflow-x-hidden">
       <div className="border-2 rounded p-2 border-gray-600 max-w-[900px]">
-        <Message />
-        <Message />
-        <Message />
-        <Message />
+        {messages.slice(0, visibleMessages)}
+        {visibleMessages < totalMessages ? (
+          <button className="btn rounded-lg hover" onClick={handleSeeMore}>
+            See more comments
+          </button>
+        ) : (
+          <button className="btn" onClick={handleShowLess}>
+            Show less
+          </button>
+        )}
       </div>
       <div className="chat chat-start">
         <form className="">
