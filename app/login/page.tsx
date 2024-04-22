a310c598f29fedee3c19bbbceb04ab4c375eb437
Abrham <abrsh6266@gmail.com>
2024-04-22 09:05:57 -0800

    some updates on login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -7,21 +7,19 @@ import Link from "next/link";
 
 const login = () => {
   return (
-    <div className="py-26">
-      <div className="container px-4 mx-auto items-center">
-        <div className="md:max-w-lg max-w-screen-xl md:mx-auto sm:w-full">
-          <form
+    <div className="h-screen grid place-items-center">
+      <form
         method="post"
         className="card w-96 gap-y-4 shadow-lg flex flex-col bg-base-100 p-4"
       >
         <Image
-                style={{
-                  maxWidth: "100%",
-                  height: "auto",
-                }}
-                src={logo}
-                alt="logo"
-              />
+          style={{
+            maxWidth: "100%",
+            height: "auto",
+          }}
+          src={logo}
+          alt="logo"
+        />
         <h4 className="text-center  text-3xl font-bold">Login</h4>
         <FormInput
           type={"email"}
@@ -43,11 +41,11 @@ const login = () => {
           <Link
             href={"/signup"}
             className="ml-2 link link-hover link-primary capitalize"
-          >Register</Link>
+          >
+            Register
+          </Link>
         </p>
       </form>
-        </div>
-      </div>
     </div>
   );
 };
