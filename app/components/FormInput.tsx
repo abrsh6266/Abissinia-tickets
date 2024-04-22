b27003ec2c306755e6c402b3744d50bfb2e1fc04
Abrham <abrsh6266@gmail.com>
2024-04-22 08:33:36 -0800

    adding form input component

---
+++ app/components/FormInput.tsx
@@ -0,0 +1,28 @@
+const FormInput = ({
+  label,
+  name,
+  type,
+  defaultValue,
+  size,
+}: {
+  label: string;
+  name: string;
+  type: string;
+  defaultValue: any;
+  size: string;
+}) => {
+  return (
+    <div className="form-control">
+      <label htmlFor={name} className="label">
+        <span className="label-text capitalize">{label}</span>
+      </label>
+      <input
+        type={type}
+        name={name}
+        defaultValue={defaultValue}
+        className={`input input-bordered ${size}`}
+      />
+    </div>
+  );
+};
+export default FormInput;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import logo from "/public/images/logo.png";
+import logo from "/public/images/logo2.png";
 
 const login = () => {
   return (
