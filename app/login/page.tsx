a5e4e70f08f26cca36f1dfe433a56b406e7c5dc5
Abrham <abrsh6266@gmail.com>
2024-04-22 09:01:42 -0800

    register and login form update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,17 +1,27 @@
 "use client";
 import Image from "next/image";
-import logo from "/public/images/logo2.png";
-import Link from "next/link";
+import logo from "/public/images/logo.png";
 import FormInput from "../components/FormInput";
 import SubmitBtn from "../components/SubmitBtn";
+import Link from "next/link";
 
 const login = () => {
   return (
-    <section className="h-screen grid place-items-center">
-      <form
+    <div className="py-26">
+      <div className="container px-4 mx-auto items-center">
+        <div className="md:max-w-lg max-w-screen-xl md:mx-auto sm:w-full">
+          <form
         method="post"
         className="card w-96 gap-y-4 shadow-lg flex flex-col bg-base-100 p-4"
       >
+        <Image
+                style={{
+                  maxWidth: "100%",
+                  height: "auto",
+                }}
+                src={logo}
+                alt="logo"
+              />
         <h4 className="text-center  text-3xl font-bold">Login</h4>
         <FormInput
           type={"email"}
@@ -31,12 +41,14 @@ const login = () => {
         <p className="text-center">
           Not a member yet?{" "}
           <Link
-            href={"/register"}
+            href={"/signup"}
             className="ml-2 link link-hover link-primary capitalize"
           >Register</Link>
         </p>
       </form>
-    </section>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 import Image from "next/image";
 import logo from "/public/images/logo.png";
+import Link from "next/link";
+import SubmitBtn from "../components/SubmitBtn";
+import FormInput from "../components/FormInput";
 
 const SignUp = () => {
   return (
     <div className="min-h-screen bg-black text-gray-100 flex justify-center">
-      <div className="max-w-screen-xl m-0 sm:m-10 bg-gradient-to-t from-black via-gray-500 to-black bg-opacity-40 shadow sm:rounded-lg flex justify-center flex-1 flex-col md:flex-row">
+      <div className="max-w-screen-xl m-0 sm:m-10 bg-gradient-to-t bg-opacity-40 shadow sm:rounded-lg flex justify-center flex-1 flex-col md:flex-row">
         <div className="md:w-1/2 md:h-1/2 lg:h-full p-6 sm:p-12 flex justify-center">
           <div className="text-center px-8 py-12">
             <a className="mb-6" href="#">
@@ -21,43 +24,40 @@ const SignUp = () => {
           </div>
         </div>
         <div className="md:w-1/2 p-6 md:p-12">
-          <form action="" className="flex flex-col justify-center px-8 py-6">
-            <div className="mb-6">
-              <input
-                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-                type="text"
-                placeholder="Full Name"
-              />
+          <form
+            method="post"
+            className="card w-96 p-8 bg-base-100 shadow-lg flex flex-col gap-y-4"
+          >
+            <h4 className="text-center text-3xl font-bold">Register</h4>
+            <FormInput
+              type={"text"}
+              label={"username"}
+              name={"username"}
+              defaultValue={""}
+            />
+            <FormInput
+              type={"email"}
+              label={"email"}
+              name={"email"}
+              defaultValue={""}
+            />
+            <FormInput
+              type={"password"}
+              label={"password"}
+              name={"password"}
+              defaultValue={""}
+            />
+            <div className="mt-4">
+              <SubmitBtn text={"register"} />
             </div>
-            <div className="mb-6">
-              <input
-                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-                type="email"
-                placeholder="Email"
-              />
-            </div>
-            <div className="mb-6">
-              <input
-                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-                type="password"
-                placeholder="Password"
-              />
-            </div>
-            <div className="mb-6">
-              <input
-                className="w-full p-4 leading-6 text-lg font-extrabold placeholder-gray-500 bg-white shadow border-2 border-gray-500 rounded"
-                type="password"
-                placeholder="Confirm Password"
-              />
-            </div>
-            <button className="py-4 px-6 mb-6 text-center text-lg leading-6 text-white font-extrabold bg-black hover:bg-gray-600 border-3 border-indigo-900 shadow rounded transition duration-200">
-              Sign Up
-            </button>
-            <p className="text-center font-extrabold">
-              Already have an account?{" "}
-              <a className="text-blue-500 hover:underline" href="/login">
-                Login here
-              </a>
+            <p className="text-center">
+              Already a member?
+              <Link
+                className="'ml-2 link link-hover link-primary capitalize"
+                href={"/login"}
+              >
+                login
+              </Link>
             </p>
           </form>
         </div>
