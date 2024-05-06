42b446ded5bee5cc28834a444bf0cf71e16f7c76
Abrham <abrsh6266@gmail.com>
2024-05-05 22:38:50 -0800

    some update

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,7 +1,6 @@
 "use client";
 import Link from "next/link";
 import { links, socials } from "../data";
-import { useGlobalContext } from "../context";
 import { usePathname } from "next/navigation";
 
 const Footer = () => {
