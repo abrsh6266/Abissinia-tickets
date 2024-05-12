20f30ee145b236da7d86bec6416116e53b419235
Abrham <abrsh6266@gmail.com>
2024-05-12 12:27:30 -0800

    resetting

--- app/components/profileComponents/Aside.tsx
+++ app/components/profileComponents/Aside.tsx
@@ -1,7 +1,7 @@
 const Aside = () => {
   return (
     <div className="sticky flex flex-col gap-2 p-4 text-sm md:border-r ">
-      <h2 className="pl-3 mb-4 text-2xl font-semibold">settings</h2>
+      <h2 className="pl-3 mb-4 text-2xl font-semibold">Settings</h2>
 
       <a
         href="#"
