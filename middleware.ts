d48a71e6f44b192b2f87396488b0c028a56ff89e
Abrham <abrsh6266@gmail.com>
2024-05-24 21:26:54 -0800

    some changes

--- middleware.ts
+++ middleware.ts
@@ -2,13 +2,12 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import axios from "axios";
 
+
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get("token");
-  console.log("token ",token);
-  if (!token) {
+  if (!token?.value) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
-
   try {
     const response = await axios.post(
       "https://abissinia-backend.vercel.app/api/auth/verify-token",
