ae635d4e6d5f7969bc1234623ee437a667aff73e
Abrham <abrsh6266@gmail.com>
2024-05-27 12:18:01 -0800

    updated schedule

--- app/film-showtimes-tickets/page.tsx
+++ app/film-showtimes-tickets/page.tsx
@@ -1,59 +1,23 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SectionTitle from "../components/common/SectionTitle";
-import { movie } from "../data";
 import MovieDetail from "../components/movieDetailComponents/MovieDetail";
 import PaginationContainer from "../components/common/PaginationContainer";
+import { Movie2 } from "../data";
+import useFetchData from "@/api/getData";
 
 const Page = () => {
-  const [movies, setMovies] = useState(movie);
-  const handleFilter = (e: any) => {
-    if (e.target.value != "") {
-      const filteredMovies = movie.filter((m) =>
-        m.showTime.some((s) => s.day === e?.target?.value)
-      );
-      setMovies(filteredMovies);
-    } else setMovies(movie);
-  };
+  const { data, isLoading, isError } = useFetchData("movies/scheduled");
+  const [movies, setMovies] = useState<Movie2[]>([]);
+  useEffect(() => {
+    if (data) {
+      setMovies(data);
+    }
+  }, [data]);
   return (
     <div className="align-element">
       <div className="sm:flex md:grid md:grid-cols-2">
         <SectionTitle text={"showTimes"} />
-        <div className="md:m-auto ml-2 relative h-10 w-72 min-w-[200px]">
-          <select
-            onChange={handleFilter}
-            className="h-full w-full rounded-[7px]  disabled:border-0 select select-bordered"
-            style={{ color: "#fff" }}
-          >
-            <option value="">none</option>
-            <option value="Monday">
-              Monday
-            </option>
-            <option value="Tuesday">
-              Tuesday
-            </option>
-            <option value="Wednesday">
-              Wednesday
-            </option>
-            <option value="Thursday">
-              Thursday
-            </option>
-            <option value="Friday">
-              Friday
-            </option>
-            <option value="Saturday">
-              Saturday
-            </option>
-            <option value="Sunday">
-              Sunday
-            </option>
-          </select>
-          <label
-            className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-gray-900 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-gray-900 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-gray-900 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500"
-          >
-            Select Day ...
-          </label>
-        </div>
       </div>
       <div className="md:text-2xl text-gray-200 sm:text-xl italic lg:mx-20">
         <p>
@@ -65,7 +29,7 @@ const Page = () => {
       </div>
       <div className="min-h-8 max-h-full m-4">
         {movies.map((m) => {
-          return <MovieDetail key={m.id} movie={m} />;
+          return <MovieDetail key={m._id} movie={m} />;
         })}
       </div>
       <div className="align-element">
--- app/schedule/page.tsx
+++ app/schedule/page.tsx
@@ -1,25 +1,80 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import { Movie2, shuffleArray } from "../data";
+import { Movie2, ShowTime, shuffleArray } from "../data";
 import MovieCard2 from "../components/movieDetailComponents/MovieCard2";
 import Loading from "../components/common/Loading";
 import useFetchData from "@/api/getData";
-
-const Schedule = () => {
-  const { data, isLoading, isError } = useFetchData("movies");
-
-  const selectFiveMovies = (data: Movie2[]): Movie2[] => {
-    const shuffledMovies = shuffleArray(data);
-    return shuffledMovies.slice(0, 6);
+import { customFetch2 } from "../utils";
+const daysOfWeek = [
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday",
+];
+interface Hall {
+  _id: string;
+  name: string;
+  seats: {
+    sofa: any[]; // You might want to create an interface for seats
+    standard: any[]; // You might want to create an interface for seats
+    premier: any[]; // You might want to create an interface for seats
   };
+}
 
-  const [movies, setMovies] = useState<Movie2[]>([]);
+interface MovieShow {
+  _id: string;
+  createdAt: string;
+  hallId: Hall;
+  movieId: Movie2;
+  showTime: ShowTime[];
+}
+interface Movie extends Movie2 {
+  showTime: MovieShow[];
+}
+const Schedule = () => {
+  const { data, isLoading, isError } = useFetchData("movies/scheduled");
+  const [selectedDay, setSelectedDay] = useState<string>("Monday");
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
 
   useEffect(() => {
     if (data && Array.isArray(data)) {
-      setMovies(selectFiveMovies(data));
+      const fetchShowTimes = async () => {
+        if (!data.length) return;
+
+        const promises = data.map(async (movie) => {
+          const response = await customFetch2(
+            `/movie-shows/by-movie/${movie._id}`
+          );
+          const result = await response.data;
+          console.log(result);
+          return { ...movie, showTime: result };
+        });
+
+        const moviesWithShowTimes = await Promise.all(promises);
+        setMovies(moviesWithShowTimes);
+      };
+
+      fetchShowTimes();
     }
   }, [data]);
+  useEffect(() => {
+    const filterMoviesByDay = () => {
+      const filtered = movies.filter(
+        (movie) =>
+          movie.showTime &&
+          movie.showTime.some((show) =>
+            show.showTime.some((week) => week.day === selectedDay)
+          )
+      );
+      setFilteredMovies(filtered);
+    };
+
+    filterMoviesByDay();
+  }, [selectedDay, movies]);
 
   if (isLoading) {
     return (
@@ -30,17 +85,24 @@ const Schedule = () => {
   }
 
   if (isError) {
-    return <div>Error loading data</div>; // Show an error state
+    return <div>Error loading data</div>;
   }
+
   return (
     <div className="align-element2">
       <div className="">
         <div className="flex border-2 border-blue-700 mx-auto ">
-          <input
-            type="date"
-            placeholder="Search Something..."
+          <select
+            value={selectedDay}
+            onChange={(e) => setSelectedDay(e.target.value)}
             className="input input-bordered w-full outline-none text-sm px-4 py-3"
-          />
+          >
+            {daysOfWeek.map((day) => (
+              <option key={day} value={day}>
+                {day}
+              </option>
+            ))}
+          </select>
           <button
             type="button"
             className="flex items-center justify-center bg-blue-700 px-5 text-sm"
@@ -50,9 +112,9 @@ const Schedule = () => {
         </div>
       </div>
       <div className="my-5 mx-auto grid grid-cols-1 gap-1 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 2xl:grid-cols-5 md:gap-4">
-        {movies.map((movie) => {
-          return <MovieCard2 {...movie} key={movie._id} />;
-        })}
+        {filteredMovies.map((movie) => (
+          <MovieCard2 {...movie} key={movie._id} />
+        ))}
       </div>
     </div>
   );
