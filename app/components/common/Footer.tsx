f6bf0f9933735f4a4820026139c2491f065035c4
Abrham <abrsh6266@gmail.com>
2024-06-17 06:08:02 -0800

    little updates

--- app/components/common/Footer.tsx
+++ app/components/common/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   const currentPath = usePathname();
   return (
     <>
-      <section className="border-t-2">
+      <section className="border-t-2 mt-24">
         <div className="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
           <nav className="flex flex-wrap justify-center -mx-5 -my-2">
             {links.map((link, index) => {
